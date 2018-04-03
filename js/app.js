0d576566873260582aa179b64390673abd008337
Jim McConnell <jimwmcconnell@yahoo.com>
2018-04-03 08:27:08 -0500

    Changed js changing ternary to if - else

--- js/app.js
+++ js/app.js
@@ -2,5 +2,9 @@ $('#boxes').on('click', function(event){
 	var listClasses = event.target.parentNode.className;
 	var boxColor = listClasses.split(' ')[0];
 
-	boxColor ? $("ul").attr("class", boxColor) : $("ul").removeAttr("class");
+	if (boxColor) {
+  	$("ul").attr("class", boxColor);
+  } else {
+  	$("ul").removeAttr("class");
+  }
 });
